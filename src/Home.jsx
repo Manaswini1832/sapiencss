f0c1a8e5ade5c8e150cab864b0f5f6bd20352bf3
Manaswini1832 <munugurimanaswini@gmail.com>
2025-05-20 22:37:48 +0530

    replace new function with iframge

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,7 @@ export const Home = () => {
     const [result, setResult] = useState("");
     const [input, setInput] = useState("MAKE RECTANGLE \"r1\" WITH color \"teal\", x \"10\", y \"100\", width \"500\", height \"100\";\nMAKE CIRCLE \"c1\" WITH color \"pink\", x \"50\", y \"50\", radius \"30\";\nMAKE LINE \"l1\" WITH color \"orange\", x \"1\", y \"15\", length \"1500\";\n"); // input state
     const [loading, setLoading] = useState(true);
+    const [htmlString, setHtmlString] = useState('data:text/html,<html><body><canvas id="canvas" width="1500" height="1500"/></body></html>');
 
     useEffect(() => {
         if (wasmModule) {
@@ -14,23 +15,17 @@ export const Home = () => {
     }, [wasmModule]);
 
     useEffect(() => {
-        if(result){
-            if(result.search("function draw") === -1){
-                let clearCanvas = new Function('function draw(){const canvas = document.getElementById("canvas");if (canvas.getContext){const ctx = canvas.getContext("2d");ctx.clearRect(0, 0, canvas.width, canvas.height);}}draw();');
-                clearCanvas();
-                alert(result);
-            }else{
-                try {
-                    let drawSapCSSCanvas = new Function(result);
-                    drawSapCSSCanvas();   
-                } catch (error) {
-                    alert(error);
-                }
-            }
+        if(result.search("function draw") === -1){
+            //clear the canvas
+            setHtmlString(htmlString + '<script>function draw(){const canvas = document.getElementById("canvas");if (canvas.getContext){const ctx = canvas.getContext("2d");ctx.clearRect(0, 0, canvas.width, canvas.height);}}draw();</script>');
+        }else{
+            //draw on the canvas
+            setHtmlString(htmlString + "<script>" + result + "</script>");
         }
     }, [result]);
 
     const handleClick = () => {   
+        setHtmlString('data:text/html,<html><body><canvas id="canvas" width="1500" height="1500"/></body></html><script>function draw(){const canvas = document.getElementById("canvas");if (canvas.getContext){const ctx = canvas.getContext("2d");ctx.clearRect(0, 0, canvas.width, canvas.height);}}draw();</script>');
         if (input && wasmModule) {
             const compileInput = input.endsWith('\n') ? input : input + '\n';
 
@@ -65,7 +60,7 @@ export const Home = () => {
             <button onClick={handleClick}>Compile</button>
             <h3>Compiler Output:</h3>
             <pre>{result}</pre>
-            <canvas id="canvas" width="1500" height="1500"/>
+            <iframe id="iframe" src={htmlString}></iframe>
         </div>
     );
 };
